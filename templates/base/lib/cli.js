3b3d7b22e80650d8bc1fa4617ecda120eb6e39fc
Mayank Lahiri <mayank@iceroad.io>
2020-05-11 23:10:06 -0700

    fix: require global

--- templates/base/lib/cli.js
+++ templates/base/lib/cli.js
@@ -2,6 +2,6 @@
 /* cli-gen: auto-generated, do not edit. Remove this line to disable template updates. */
 
 const program = require("commander");
-require("../global");
+require("./global");
 require("./verbs")(program);
 program.parse(process.argv);
