d823fa3d84835d7779a636481b9f0c294fd39023
Mayank Lahiri <mayank@iceroad.io>
2020-05-24 22:07:42 -0700

    [template] Simplify CLI entry point.

--- templates/base/lib/cli.js
+++ templates/base/lib/cli.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 /* cli-gen: auto-generated, do not edit. Remove this line to disable template updates. */
-
-const program = require("commander");
 require("./global");
-require("./verbs")(program);
-program.parse(process.argv);
+require("./verbs")(require("commander")).parse(process.argv);
--- templates/base/lib/verbs/index.js
+++ templates/base/lib/verbs/index.js
@@ -1,12 +1,13 @@
 /* cli-gen: auto-generated, do not edit. Remove this line to disable template updates. */
-
-const path = require("path");
 const fs = require("fs");
+const path = require("path");
 
-module.exports = program =>
+module.exports = program => {
   fs.readdirSync(__dirname).forEach(filename => {
     const absPath = path.join(__dirname, filename);
     if (fs.statSync(absPath).isDirectory()) {
       require(absPath)(program);
     }
   });
+  return program;
+};
